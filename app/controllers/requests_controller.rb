59307a1a75ed4870227d2aaf2b37dd8760084b6b
Ryan Kayu Ching <ryanching@hotmail.com>
2016-02-09 12:39:07 -0800

    Add undone/done button

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -27,7 +27,7 @@ class RequestsController < ApplicationController
   def update
     request = Request.find params[:id]
     request.update request_params
-    redirect_to requests_path
+    redirect_to request_path(request)
   end
 
   def destroy
